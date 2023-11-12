dbe5cab4dc9ea9874048c76aa12057b94faaf62d
Gareth Ng <njwqz77@gmail.com>
2023-11-12 22:08:22 +0800

    Initial Commit

---
+++ weather/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  weather
+//
+//  Created by Gareth Ng on 2023/11/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ weather/weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherApp.swift
+//  weather
+//
+//  Created by Gareth Ng on 2023/11/12.
+//
+
+import SwiftUI
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
